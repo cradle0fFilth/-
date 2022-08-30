a66e3570cbf36529085ba0d6ad7d6a4260475567
0xathena <3175806342@qq.com>
2022-08-30 15:49:42 +0800

    Delete annotation.

--- private.js
+++ private.js
@@ -3,7 +3,7 @@ var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/'
 var util = require('ethereumjs-util');
 //var tx = require('ethereumjs/tx');
 
-//let privateKey = util.toBuffer("0xc0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0de");
+
 let privateKey = util.toBuffer("0x1111111111111111111111111111111111111111111111111111111111111111");
 //var publicKey = util.bufferToHex(util.privateToPublic(privateKey));
 var publicKey = util.privateToPublic(privateKey);
